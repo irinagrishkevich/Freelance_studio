610726c239ec33773dceac62d081cd6f6090d835
irinagrishkevich <bonivomal1996@gmail.com>
2024-05-27 20:51:22 +0200

    add view freelancer

--- frontend/src/components/freelancers/freelancers-list.js
+++ frontend/src/components/freelancers/freelancers-list.js
@@ -1,5 +1,6 @@
 import {HttpUtils} from "../../utils/http-utils";
 import config from "../../config/config";
+import {CommonUtils} from "../../utils/common-utils";
 
 export class FreelancersList{
     constructor(openNewRoute){
@@ -28,28 +29,14 @@ export class FreelancersList{
             trElement.insertCell(1).innerHTML = freelancers[i].avatar ? '<img class="freelancer-avatar" src="' + config.host + freelancers[i].avatar + '" alt="User Image" ">' : ''
             trElement.insertCell(2).innerText = freelancers[i].name + ' ' + freelancers[i].lastName
             trElement.insertCell(3).innerText = freelancers[i].email
-            let levelHtml = null
-            switch (freelancers[i].level) {
-                case config.freelancerLevels.junior:
-                    levelHtml = '<span class="badge badge-info">Junior</span>'
-                    break
-                case config.freelancerLevels.middle:
-                    levelHtml = '<span class="badge badge-warning">Middle</span>'
-                    break
-                case config.freelancerLevels.senior:
-                    levelHtml = '<span class="badge badge-success">Senior</span>'
-                    break
-                default:
-                    levelHtml = '<span class="badge badge-secondary">Unknown</span>'
-            }
-            trElement.insertCell(4).innerHTML = levelHtml
+            trElement.insertCell(4).innerHTML  = CommonUtils.getLevelHtml(freelancers[i].level)
             trElement.insertCell(5).innerText = freelancers[i].education
             trElement.insertCell(6).innerText = freelancers[i].location
             trElement.insertCell(7).innerText = freelancers[i].skills
             trElement.insertCell(8).innerHTML = '<div class="freelancer-tools"">' +
-                '<a href="/freelancers/view?id="' + freelancers[i].id + ' class="fas fa-eye"></a>' +
-                '<a href="/freelancers/edit?id="' + freelancers[i].id + ' class="fas fa-edit"></a>' +
-                '<a href="/freelancers/delete?id="' + freelancers[i].id + ' class="fas fa-trash"></a>' +
+                '<a href="/freelancers/view?id=' + freelancers[i].id + '" class="fas fa-eye"></a>' +
+                '<a href="/freelancers/edit?id=' + freelancers[i].id + '" class="fas fa-edit"></a>' +
+                '<a href="/freelancers/delete?id=' + freelancers[i].id + '" class="fas fa-trash"></a>' +
                 '</div>'
             recordsElement.appendChild(trElement)
         }
